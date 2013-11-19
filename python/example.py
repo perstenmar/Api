79c0a93f4f34f208792a7c50ff0fa381fb17df47
Henrik Hussfelt <henrik@coursio.com>
2013-11-19 12:03:03 +0100

    Adding example python file for get functions

---
+++ python/example.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+from CoursioApi import CoursioApi
+coursio = CoursioApi('6bee83c2-75a5-4400-aff3-a3315403b896', '$1$VgoLUt/N$27QP.Z8K8ThiRsTqcNOyV1', 'pepper')
+print coursio.get('/v1/invitations');
